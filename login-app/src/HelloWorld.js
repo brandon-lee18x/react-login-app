0722932eaa36b1eb7fd35dbd4f9805224c74fb6f
Brandon Lee <72825076+brandon-lee18x@users.noreply.github.com>
2021-12-25 21:17:51 -0800

    Added text boxes

---
+++ login-app/src/HelloWorld.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  
+  return (
+    <body>
+    <p>Email</p>
+    <input id = "EmailBox" placeholder = "Email"></input>
+    <br></br>
+    <p>Password</p>
+    <input id = "PasswordBox" placeholder = "Password"></input>
+    
+    
+    </body>
+  );
+};
+
+export default HelloWorld;
